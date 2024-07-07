ef4bb1bdbe28ea9e795d4366475027c51ce071ce
sharif-jobayed <sharif.jobayed@gmail.com>
2024-07-07 10:27:20 +0600

    Remove unnecessary imports

--- src/test/java/com/theInternetHerokuApp/pages/HomePage.java
+++ src/test/java/com/theInternetHerokuApp/pages/HomePage.java
@@ -1,10 +1,6 @@
 package com.theInternetHerokuApp.pages;
 
-import com.theInternetHerokuApp.framework.BaseElement;
 import com.theInternetHerokuApp.framework.BasePage;
-import org.openqa.selenium.By;
-
-import static com.theInternetHerokuApp.data.Constants.getBaseURL;
 
 public class HomePage extends BasePage {
     public HomePage(String pageURL, String pageName) {
