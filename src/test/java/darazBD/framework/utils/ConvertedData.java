b9277cc45a6ae68011580264a3aa16018e78f809
sharif-jobayed <sharif.jobayed@gmail.com>
2024-09-04 04:11:34 +0600

    Set up data converter

--- src/test/java/darazBD/framework/utils/ConvertedData.java
+++ src/test/java/darazBD/framework/utils/ConvertedData.java
@@ -1,4 +1,45 @@
 package darazBD.framework.utils;
 
+import java.util.Map;
+
 public class ConvertedData {
+    private String BaseURL;
+    private Map<String, User> Users;
+
+    public String getBaseURL() {
+        return BaseURL;
+    }
+
+    public void setBaseURL(String baseURL) {
+        this.BaseURL = baseURL;
+    }
+
+    public Map<String, User> getUsers() {
+        return Users;
+    }
+
+    public void setUsers(Map<String, User> users) {
+        this.Users = users;
+    }
+
+    public static class User {
+        private String UserEmail;
+        private String UserPassword;
+
+        public String getUserEmail() {
+            return UserEmail;
+        }
+
+        public void setUserEmail(String userEmail) {
+            this.UserEmail = userEmail;
+        }
+
+        public String getUserPassword() {
+            return UserPassword;
+        }
+
+        public void setUserPassword(String userPassword) {
+            this.UserPassword = userPassword;
+        }
+    }
 }
--- src/test/java/darazBD/framework/utils/DataConverter.java
+++ src/test/java/darazBD/framework/utils/DataConverter.java
@@ -1,4 +1,29 @@
 package darazBD.framework.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DataConverter {
+    private final Gson GSON;
+    private final File CREDENTIALS;
+
+    public DataConverter() {
+        this.GSON = new Gson();
+        this.CREDENTIALS = new File("./src/test/java/darazBD/data/credentials.json");
+    }
+
+    public ConvertedData getCreds() {
+        try (FileReader reader = new FileReader(this.CREDENTIALS)) {
+            return this.GSON.fromJson(reader, ConvertedData.class);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Credentials file not found", e);
+        } catch (JsonSyntaxException | IOException e) {
+            throw new RuntimeException("Failed to parse credentials file", e);
+        }
+    }
 }
