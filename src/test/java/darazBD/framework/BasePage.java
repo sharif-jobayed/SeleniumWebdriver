c89d3ab3078ac98af181909ba826b02315ed78aa
sharif-jobayed <sharif.jobayed@gmail.com>
2024-09-05 07:14:29 +0600

    Setting up the framework

--- src/test/java/darazBD/framework/BasePage.java
+++ src/test/java/darazBD/framework/BasePage.java
@@ -1,4 +1,25 @@
 package darazBD.framework;
 
+import static darazBD.framework.DriverTools.getWDriver;
+
 public abstract class BasePage {
+    private String pURL;
+    private String pName;
+
+    public BasePage(String pageURL, String pageName) {
+        this.pURL = pageURL;
+        this.pName = pageName;
+    }
+
+    public String getPURL() {
+        return this.pURL;
+    }
+
+    public String getPName() {
+        return this.pName;
+    }
+
+    public String getPTitile() {
+        return getWDriver().getTitle();
+    }
 }
