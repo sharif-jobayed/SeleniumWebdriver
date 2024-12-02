b6f6eab86f057fa61f1096d87d8255f289927801
sharif-jobayed <sharif.jobayed@gmail.com>
2024-12-03 03:59:49 +0600

    Setting up the BaseElement

--- src/test/java/framework/elements/BaseElement.java
+++ src/test/java/framework/elements/BaseElement.java
@@ -1,26 +1,48 @@
 package framework.elements;
 
+import framework.utils.DataConverter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class BaseElement {
+public class BaseElement {
     private WebDriver driver;
     private By locator;
+    private DataConverter dataConverter;
 
-    protected BaseElement(By locator) {
+    public BaseElement(By locator) {
+        this.dataConverter = new DataConverter();
         this.locator = locator;
     }
 
-    protected WebElement getEl() {
+    public WebElement getElement() {
         return this.driver.findElement(this.locator);
     }
 
-    protected Boolean isElsExist() {
-        List<WebElement> elements = new ArrayList<>(this.driver.findElements(this.locator));
+    public List<WebElement> getElements() {
+        return this.driver.findElements(this.locator);
+    }
+
+    public Boolean doElementsExist() {
+        List<WebElement> elements = new ArrayList<>(this.getElements());
         return !elements.isEmpty();
+    }
+
+    public Boolean isElementVisible() {
+        return this.getElement().isDisplayed();
+    }
+
+    public Boolean isElementEnabled() {
+        return this.getElement().isEnabled();
+    }
+
+    public void waitTillVisible(Integer timeout) {
+        new WebDriverWait(this.driver, Duration.ofSeconds(timeout)).until(ExpectedConditions.visibilityOfElementLocated(this.locator));
     }
 }
