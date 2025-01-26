78e561ebeb5b915889149d62c5f03bb08e90e13b
sharif-jobayed <sharif.jobayed@gmail.com>
2025-01-27 03:23:11 +0600

    Validate that, the Dashboard page is open & loaded

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -1,10 +1,22 @@
 package pages;
 
 import framework.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DashboardPage extends BasePage {
+    private final By PROFILE_ITM;
+
     public DashboardPage(WebDriver driver) {
         super(driver);
+
+        this.PROFILE_ITM = By.xpath("//li[@class='oxd-userdropdown']");
     }
+
+    public Boolean isDashboardPageOpen() {
+        this.isPageOpen(5);
+        return this.isPageLoaded(5);
+    }
+
+
 }
--- src/test/java/tests/DashboardPageTest.java
+++ src/test/java/tests/DashboardPageTest.java
@@ -1,9 +1,17 @@
 package tests;
 
 import framework.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DashboardPage;
 
 public class DashboardPageTest extends BaseTest {
 
+    @Test(priority = 1, description = "Validate that, the Dashboard page is open & loaded", dependsOnMethods = "tests.LoginPageTest.loginWithValidCredentials")
+    public void isDashboardPageOpen() {
+        DashboardPage dashboardPage = new DashboardPage(this.driver);
 
+        Assert.assertTrue(dashboardPage.isDashboardPageOpen(), "The Dashboard page isn't open");
+    }
 
 }
