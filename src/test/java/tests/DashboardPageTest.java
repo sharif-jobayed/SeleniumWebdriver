3e506baf7a982ccb544cc51ae705e15efa7d03b6
sharif-jobayed <sharif.jobayed@gmail.com>
2025-02-12 06:49:30 +0600

    Remove debugging code

--- src/test/java/tests/DashboardPageTest.java
+++ src/test/java/tests/DashboardPageTest.java
@@ -13,18 +13,11 @@ public class DashboardPageTest extends BaseTest {
             dependsOnMethods = {"tests.LoginPageTest.loginToAppWithValidCredentials"}
     )
     public void appLanguageIsEnglish() {
-        System.out.println("Entered in the appLanguageIsEnglish...");
         DashboardPage dashboardPage = this.getPages().getDashboardPage();
-        System.out.println("Dashboard page is instantiated...");
-
         dashboardPage.isPageOpen(1);
-        System.out.println("Dashboard page is open...");
-
         dashboardPage.isPageLoaded(3);
-        System.out.println("Dashboard page is loaded...");
 
         Assert.assertEquals(dashboardPage.getApplicationHeaderTitle(), "Dashboard", "The application isn't in English");
-        System.out.println("Assert complete...");
     }
 
 }
